6bd6794deac06469e0f793bfa76b37446f63913a
Umang Saxena <hanusaxena68@gmail.com>
2023-08-14 21:56:55 +0530

    UseLocation hook added to chage active link in Nav

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,9 @@
-import React,{ useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
+import React  from 'react'
+// import noteContext from '../context/notes/noteContext'
 const About = () => {
-    const a=useContext(noteContext)
-    useEffect(() =>{
-        a.update()
-        // eslint-disable-next-line
-    },[])
   return (
     <div>
-      About {a.state.Name} and he is in class {a.state.Class}
+      This is About Page
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,25 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NoteContext from './noteContext'
 
 const NoteState=( props )=>{
-    const s1={
-        "Name":"Umang",
-        "Class":"5B"
-    }
-    const [state,setState]=useState(s1);
-    const update=()=>{
-        setTimeout(() => {
-            setState(
-                {
-                    "Name":"Hanu Saxena",
-                    "Class":"10A"
-                }
-            )
-        }, 1000);
-    }
-
+   
     return (
-            <NoteContext.Provider value={{state,update}} >  {/* It is same as value={{state:state,update:update}} */}
+            <NoteContext.Provider value={{  }} > 
                 { props.children }
             </NoteContext.Provider>
 
