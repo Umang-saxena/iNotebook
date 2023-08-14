eea72f0959a90c003316cd47f421e66f84629e4d
Umang Saxena <hanusaxena68@gmail.com>
2023-08-14 14:53:39 +0530

    React Router Setup Successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import { BrowserRouter ,Routes,Route } from "react-router-dom";
+
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
+      <Routes>
+        <Route exact path="/" element={ <Home/> } />
+        <Route exact path="/about" element={ <About/> } />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
