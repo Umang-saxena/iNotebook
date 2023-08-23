ae6a35dd79ed44056e35fc1596059f360aabf89f
Umang Saxena <hanusaxena68@gmail.com>
2023-08-23 10:33:52 +0530

    Logout  Button Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React,{ useEffect } from 'react';
-import { Link,useLocation } from "react-router-dom";
+import { Link,useLocation, useNavigate } from "react-router-dom";
 
 
 const Navbar = () => {
   let location = useLocation();
+  let navigate=useNavigate();
+  const handleLogout=()=>{
+    localStorage.removeItem('token');
+    navigate('/login');
+
+  }
 
   useEffect(() => {
   }, [location]);
@@ -24,12 +30,10 @@ const Navbar = () => {
         </li>
         
       </ul>
-      <form className="d-flex" role="search">
-        {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button> */}
+      { localStorage.getItem('token')?<button className="btn btn-primary" onClick={handleLogout} >Logout</button>: <form className="d-flex" role="search">
         <Link className="btn btn-primary mx-2" to="/login" role="button">Login</Link>
       <Link className="btn btn-primary " to="/signup" role="button">Sign Up</Link>
-      </form>
+      </form>}
     </div>
   </div>
 </nav>
